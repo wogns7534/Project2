7584f598fb69a8f354a45b8eaf8c95ddcf5ac509
pdache <37354733+pdache@users.noreply.github.com>
2020-05-18 12:59:39 +0900

    5번 기능 코드 구현
    5번 기능 코드 구현 완료

--- main.c
+++ main.c
@@ -29,22 +29,34 @@ int main(){
 
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1(){
-    
+	const char* filename = "./text1";
+
+	if (stat(filename, &stat1)) {
+		printf("This file does not exist.\n");
+	}
 }
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(){
-    
+	const char* filename = "./text2";
+
+	if (stat(filename, &stat2)) {
+		printf("This file does not exist.\n");
+	}
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    
+	static struct tm time_1;
+	time_1 = *(localtime(&stat1.st_mtime));
+	time1 = &time_1;
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    
+	static struct tm time_2;
+	time_2 = *(localtime(&stat2.st_mtime));
+	time2 = &time_2;
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
@@ -59,6 +71,19 @@ void blockcmp(){
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
+
+	if (time1->tm_mon < time2->tm_mon)
+		printf("text1 early modified.\n");
+	else if (time1->tm_mon > time2->tm_mon)
+		printf("text2 early modified.\n");
+	else {											//같은 달에 만들어졌을 때 비교
+		if (time1->tm_mday < time2->tm_mday)
+			printf("text1 early modified.\n");
+		else if (time1->tm_mday > time2->tm_mday)
+			printf("text2 early modified.\n");
+		else
+			printf("same date modified.\n");
+	}
     
 }
 
